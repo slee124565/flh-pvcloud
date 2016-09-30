5157f2f4315e8eab8bfe5c2c68e8881cedb0a565
Lee Shiueh <lee.shiueh@gmail.com>
2016-09-30 11:32:50 +0800

    add admin_view

---
+++ pvs/views_admin.py
@@ -0,0 +1,64 @@
+from django.http import HttpResponse
+from .models import *
+
+import json
+
+class PvsManager:
+    
+    @classmethod
+    def get_serial_list(cls):
+        '''return a list of distinct pvs serial from pvs_report table
+        ex: 
+            [('serial','address'), ...]
+        '''
+        serial_list = [{'address': json.loads(entry.dbconfig).get('accuweather').get('address'),
+                        'serial': entry.serial,
+                        'local ip': entry.local_ip,
+                        'public ip': entry.ip} for entry in Report.objects.all()]
+        return serial_list
+    
+    @classmethod
+    def get_today_energy_report(cls):
+        '''return today energy report json data
+        ex:
+            {
+                'date' : 'YYYY-mm-dd',
+                'pvs' : [
+                    {
+                        'serial': 'xxxx',
+                        'pvi': [
+                            {
+                                'modbus_id': xxx,
+                                'energy': {
+                                    'current': {
+                                        'count': xxx,
+                                        'not_zero': xxx
+                                    }
+                                }
+                            }, ...
+                        ]
+                    }, ...
+                ]
+            }
+        '''
+        energy_report = {}
+        #TODO:
+        return energy_report
+    
+class ConsoleHttpResponse(HttpResponse):
+    
+    def __init__(self,request):
+        super(ConsoleHttpResponse,self).__init__()
+        self.content_type = 'plain/text'
+        
+        serial_list = PvsManager.get_serial_list()
+        energy_data = PvsManager.get_today_energy_report()
+        
+        self.content = ''
+        for entry in serial_list:
+            self.content += entry.get('address') + '\r\n' + json.dumps(entry,indent=4)
+            
+        self.content += json.dumps(energy_data,indent=4)
+        
+def admin_view(request):
+    return ConsoleHttpResponse()
