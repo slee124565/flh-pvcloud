e7a23580209e4d37d8187b94acb9113c6b4bc166
Lee Shiueh <lee.shiueh@gmail.com>
2016-10-03 16:03:28 +0800

    revise amchart template and add sample data

--- webapp/app/js/pvstation.js
+++ webapp/app/js/pvstation.js
@@ -1,143 +1,189 @@
-var pvs_data_en_hourly = [ {
-    "country": "USA",
-    "visits": 2025
-  }, {
-    "country": "China",
-    "visits": 1882
-  }, {
-    "country": "Japan",
-    "visits": 1809
-  }, {
-    "country": "Germany",
-    "visits": 1322
-  }, {
-    "country": "UK",
-    "visits": 1122
-  }, {
-    "country": "France",
-    "visits": 1114
-  }, {
-    "country": "India",
-    "visits": 984
-  }, {
-    "country": "Spain",
-    "visits": 711
-  }, {
-    "country": "Netherlands",
-    "visits": 665
-  }, {
-    "country": "Russia",
-    "visits": 580
-  }, {
-    "country": "South Korea",
-    "visits": 443
-  }, {
-    "country": "Canada",
-    "visits": 441
-  }, {
-    "country": "Brazil",
-    "visits": 395
-  } ];
-
-var pvs_data_en_daily = [ {
-    "country": "USA",
-    "visits": 2025
-  }, {
-    "country": "China",
-    "visits": 1882
-  }, {
-    "country": "Japan",
-    "visits": 1809
-  }, {
-    "country": "Germany",
-    "visits": 1322
-  }, {
-    "country": "UK",
-    "visits": 1122
-  }, {
-    "country": "France",
-    "visits": 1114
-  }, {
-    "country": "India",
-    "visits": 984
-  }, {
-    "country": "Spain",
-    "visits": 711
-  }, {
-    "country": "Netherlands",
-    "visits": 665
-  }, {
-    "country": "Russia",
-    "visits": 580
-  }, {
-    "country": "South Korea",
-    "visits": 443
-  }, {
-    "country": "Canada",
-    "visits": 441
-  }, {
-    "country": "Brazil",
-    "visits": 395
-  } ];
 
 var chart_hourly = AmCharts.makeChart("pvs_chart_en_hourly", {
-	  "type": "serial",
-	  "theme": "light",
-	  "dataProvider": pvs_data_en_hourly,
-	  "gridAboveGraphs": true,
-	  "startDuration": 1,
-	  "graphs": [ {
-	    "balloonText": "[[category]]: <b>[[value]]</b>",
-	    "fillAlphas": 0.8,
-	    "lineAlpha": 0.2,
-	    "type": "column",
-	    "valueField": "visits"
-	  } ],
-	  "chartCursor": {
-	    "categoryBalloonEnabled": false,
-	    "cursorAlpha": 0,
-	    "zoomable": false
-	  },
-	  "categoryField": "country",
-	  "categoryAxis": {
-	    "gridPosition": "start",
-	    "gridAlpha": 0,
-	    "tickPosition": "start",
-	    "tickLength": 20
-	  },
-	  "export": {
-	    "enabled": true
-	  }
+  "type": "serial",
+  "addClassNames": true,
+  "theme": "light",
+    "legend": {
+        "equalWidths": false,
+        "useGraphSettings": true,
+        "valueAlign": "left",
+        "valueWidth": 120
+    },
+  "balloon": {
+    "adjustBorderColor": false,
+    "horizontalPadding": 10,
+    "verticalPadding": 8,
+    "color": "#ffffff"
+  },
+  "balloonDateFormat" : "JJ:NN",
 
-	} );
+  "dataProvider": pvs_data_en_hourly,
+  /*
+  "dataLoader": {
+    "url": 'http://'+location.host+'/appeng/amchart/3/',
+  },*/
+  "valueAxes": [{
+    "id": "energyAxis",           
+    "axisAlpha": 0,
+     "gridAlpha":0,
+    "position": "left",
+     "title": "Energy(Wh)",},
+    {
+     "id": "uvAxis",           
+     "axisAlpha": 0,
+     "gridAlpha":0,
+     "position": "right",
+     "title": "UV",
+     
+    },
+    ],
+  "startDuration": 1,
+  "graphs": [{
+    "alphaField": "alpha",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+    "fillAlphas": 1,
+    "type": "column",
+     "title": "Energy",
+    "valueField": "energy",
+     "valueAxis": "energyAxis",
+    "dashLengthField": "dashLengthColumn"
+  }, {
+    "id": "graph3",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]](max)</span> [[additional]]</span>",
+    "bullet": "triangleUp",
+    "lineThickness": 2,
+    "bulletSize": 7,
+    "bulletBorderAlpha": 1,
+    "bulletColor": "#A9F5A9",
+                //"fillColors" : "#A9F5A9",
+    "useLineColorForBulletBorder": true,
+    "bulletBorderThickness": 3,
+    "fillAlphas": 0,
+    "lineAlpha": 1,
+    "title": "UV",
+    "valueField": "uv",
+    "valueAxis": "uvAxis",
+  }],
+  "dataDateFormat": "YYYY-MM-DD JJ:NN:SS",
+  "categoryField": "date",
+  "categoryAxis": {
+    "dateFormats": [{
+      		"period":'hh', 
+      		"format":'JJ:NN'
+    	}, {
+          	"period": 'JJ', 
+          	"format": 'JJ:NN'
+    	}, {
+            "period": "DD",
+            "format": "MMM DD"
+        }, {
+            "period": "WW",
+            "format": "MMM DD"
+        }, {
+            "period": "MM",
+            "format": "MMM"
+        }, {
+            "period": "YYYY",
+            "format": "YYYY"
+        }],
+    "parseDates": true,
+    "minPeriod" : "hh",
+    "gridPosition": "start",
+    "axisAlpha": 0,
+    "tickLength": 0
+  },
+  "export": {
+    "enabled": true
+  }
+});
 
 var chart_daily = AmCharts.makeChart("pvs_chart_en_daily", {
-	  "type": "serial",
-	  "theme": "light",
-	  "dataProvider": pvs_data_en_daily,
-	  "gridAboveGraphs": true,
-	  "startDuration": 1,
-	  "graphs": [ {
-	    "balloonText": "[[category]]: <b>[[value]]</b>",
-	    "fillAlphas": 0.8,
-	    "lineAlpha": 0.2,
-	    "type": "column",
-	    "valueField": "visits"
-	  } ],
-	  "chartCursor": {
-	    "categoryBalloonEnabled": false,
-	    "cursorAlpha": 0,
-	    "zoomable": false
-	  },
-	  "categoryField": "country",
-	  "categoryAxis": {
-	    "gridPosition": "start",
-	    "gridAlpha": 0,
-	    "tickPosition": "start",
-	    "tickLength": 20
-	  },
-	  "export": {
-	    "enabled": true
-	  }
+  "type": "serial",
+  "addClassNames": true,
+  "theme": "light",
+    "legend": {
+        "equalWidths": false,
+        "useGraphSettings": true,
+        "valueAlign": "left",
+        "valueWidth": 120
+    },
+  "balloon": {
+    "adjustBorderColor": false,
+    "horizontalPadding": 10,
+    "verticalPadding": 8,
+    "color": "#ffffff"
+  },
 
-	} );
+  "dataProvider": pvs_data_en_daily,
+  /*
+  "dataLoader": {
+    //"url": 'http://'+location.host+'/pvi/query/H5/daily/energy/',
+    "url": 'http://'+location.host+'/appeng/amchart/',
+    //"url": mytest(),
+  },*/
+  "valueAxes": [{
+    "id": "energyAxis",           
+    "axisAlpha": 0,
+     "gridAlpha":0,
+    "position": "left",
+     "title": "Energy(Wh)",},
+    /*{
+    "id": "visibility",           
+    "axisAlpha": 0,
+     "gridAlpha":0,
+    "position": "right",
+     "title": "Visibility",
+     
+    },*/
+    ],
+  "startDuration": 1,
+  "graphs": [{
+    "alphaField": "alpha",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+    "fillAlphas": 1,
+    "type": "column",
+     "title": "Energy",
+    "valueField": "energy",
+     "valueAxis": "energyAxis",
+    "dashLengthField": "dashLengthColumn"
+  }, /*{
+    "id": "graph2",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+    "bullet": "round",
+    "lineThickness": 3,
+    "bulletSize": 7,
+    "bulletBorderAlpha": 1,
+    "bulletColor": "#FFFFFF",
+    "useLineColorForBulletBorder": true,
+    "bulletBorderThickness": 3,
+    "fillAlphas": 0,
+    "lineAlpha": 1,
+    "title": "Visibility",
+    "valueField": "visibility",
+     "valueAxis": "visibilityAxis",
+  }*/],
+  "dataDateFormat": "YYYY-MM-DD",
+  "categoryField": "date",
+  "categoryAxis": {
+    "dateFormats": [{
+            "period": "DD",
+            "format": "DD"
+        }, {
+            "period": "WW",
+            "format": "MMM DD"
+        }, {
+            "period": "MM",
+            "format": "MMM"
+        }, {
+            "period": "YYYY",
+            "format": "YYYY"
+        }],
+    "parseDates": true,
+    "gridPosition": "start",
+    "axisAlpha": 0,
+    "tickLength": 0
+  },
+  "export": {
+    "enabled": true
+  }
+});
