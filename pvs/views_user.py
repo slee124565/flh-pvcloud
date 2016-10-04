f7f5bc5bdaede9b4bd28fb20b32d287d8af24ee3
Lee Shiueh <lee.shiueh@gmail.com>
2016-10-04 18:22:16 +0800

    stacked amcharts develop

--- pvs/views_user.py
+++ pvs/views_user.py
@@ -8,20 +8,28 @@ class UserPVStationView(TemplateView):
     
     template_name = 'pvstation.html'
     
-    def prepare_pvs_energy_daily_output_data(self,pvs_serial):
+    ENERGY_DATA_TYPE_TOTAL = 1
+    ENERGY_DATA_TYPE_STACKED = 2
+    
+    def prepare_pvs_energy_daily_output_data(self,pvs_serial,en_daily_data_type=ENERGY_DATA_TYPE_TOTAL):
         
         pvs_en_daily_data = Energy.get_energy_daily_output(pvs_serial)[pvs_serial]
         p_date_list = [entry for entry in pvs_en_daily_data]
         p_date_list.sort()
         
         p_data = []
-        for p_en_date in p_date_list:
-            entry_data = { 'date': p_en_date,
-                          'energy': 0}
-            for key in pvs_en_daily_data[p_en_date]:
-                if key != 'date':
-                    entry_data['energy'] += pvs_en_daily_data[p_en_date][key]
-            p_data.append(entry_data)
+        if en_daily_data_type == self.ENERGY_DATA_TYPE_TOTAL:
+            for p_en_date in p_date_list:
+                entry_data = { 'date': p_en_date,
+                              'energy': 0}
+                for key in pvs_en_daily_data[p_en_date]:
+                    if key != 'date':
+                        entry_data['energy'] += pvs_en_daily_data[p_en_date][key]
+                p_data.append(entry_data)
+        elif en_daily_data_type == self.ENERGY_DATA_TYPE_STACKED:
+            for p_en_date in p_date_list:
+                p_data.append(pvs_en_daily_data[p_en_date])
+                
         return p_data
         
     def get_context_data(self, **kwargs):
@@ -29,6 +37,8 @@ class UserPVStationView(TemplateView):
         pvs_serial = self.kwargs.get('pvs_serial')
         if pvs_serial in Energy.get_distinct_serial():
             context['pvs_serial'] = self.kwargs.get('pvs_serial')
-            pvs_en_daily = self.prepare_pvs_energy_daily_output_data(pvs_serial)
+            pvs_en_daily = self.prepare_pvs_energy_daily_output_data(pvs_serial,self.ENERGY_DATA_TYPE_STACKED)
             context['pvs_data_en_daily'] = json.dumps(pvs_en_daily)
         return context
+    
+    
--- webapp/app/js/pvstation.js
+++ webapp/app/js/pvstation.js
@@ -97,7 +97,7 @@ var chart_hourly = AmCharts.makeChart("pvs_chart_en_hourly", {
   }
 });
 
-var chart_daily = AmCharts.makeChart("pvs_chart_en_daily", {
+var chart_daily = AmCharts.makeChart("pvs_chart_en_daily_total", {
   "type": "serial",
   "addClassNames": true,
   "theme": "light",
@@ -187,3 +187,107 @@ var chart_daily = AmCharts.makeChart("pvs_chart_en_daily", {
     "enabled": true
   }
 });
+
+var chart_daily_stacked = AmCharts.makeChart("pvs_chart_en_daily", {
+	  "type": "serial",
+	  "addClassNames": true,
+	  "theme": "light",
+	    "legend": {
+	        "equalWidths": false,
+	        "useGraphSettings": true,
+	        "valueAlign": "left",
+	        "valueWidth": 120
+	    },
+	  "balloon": {
+	    "adjustBorderColor": false,
+	    "horizontalPadding": 10,
+	    "verticalPadding": 8,
+	    "color": "#ffffff"
+	  },
+
+	  "dataProvider": pvs_data_en_daily,
+	  /*
+	  "dataLoader": {
+	    //"url": 'http://'+location.host+'/pvi/query/H5/daily/energy/',
+	    "url": 'http://'+location.host+'/appeng/amchart/',
+	    //"url": mytest(),
+	  },*/
+	  "valueAxes": [{
+		"id": "energyAxis",           
+		"axisAlpha": 0,
+		"gridAlpha":0,
+		"position": "left",
+		"title": "Energy(Wh)",
+		"stackType": "regular", 
+		},
+	    /*{
+	    "id": "visibility",           
+	    "axisAlpha": 0,
+	     "gridAlpha":0,
+	    "position": "right",
+	     "title": "Visibility",
+	     
+	    },*/
+	    ],
+	  "startDuration": 1,
+	  "graphs": [{
+		    "alphaField": "alpha",
+		    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+		    "fillAlphas": 1,
+		    "type": "column",
+		    "title": "id1",
+		    "valueField": "1",
+		    "valueAxis": "energyAxis",
+		    "dashLengthField": "dashLengthColumn"
+	  },{
+		    "alphaField": "alpha",
+		    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+		    "fillAlphas": 1,
+		    "type": "column",
+		    "title": "id2",
+		    "valueField": "2",
+		    "valueAxis": "energyAxis",
+		    "dashLengthField": "dashLengthColumn"
+		  },
+	  
+	  /*{
+	    "id": "graph2",
+	    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+	    "bullet": "round",
+	    "lineThickness": 3,
+	    "bulletSize": 7,
+	    "bulletBorderAlpha": 1,
+	    "bulletColor": "#FFFFFF",
+	    "useLineColorForBulletBorder": true,
+	    "bulletBorderThickness": 3,
+	    "fillAlphas": 0,
+	    "lineAlpha": 1,
+	    "title": "Visibility",
+	    "valueField": "visibility",
+	     "valueAxis": "visibilityAxis",
+	  }*/],
+	  "dataDateFormat": "YYYY-MM-DD",
+	  "categoryField": "date",
+	  "categoryAxis": {
+	    "dateFormats": [{
+	            "period": "DD",
+	            "format": "DD"
+	        }, {
+	            "period": "WW",
+	            "format": "MMM DD"
+	        }, {
+	            "period": "MM",
+	            "format": "MMM"
+	        }, {
+	            "period": "YYYY",
+	            "format": "YYYY"
+	        }],
+	    "parseDates": true,
+	    "gridPosition": "start",
+	    "axisAlpha": 0,
+	    "tickLength": 0
+	  },
+	  "export": {
+	    "enabled": true
+	  }
+	});
