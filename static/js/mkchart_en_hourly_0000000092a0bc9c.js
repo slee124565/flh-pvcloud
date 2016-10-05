9b209df98f6d383e30bae2cd88ec2df58f91466d
Lee Shiueh <lee.shiueh@gmail.com>
2016-10-05 16:22:43 +0800

    console view draft online

---
+++ static/js/mkchart_en_hourly_0000000092a0bc9c.js
@@ -0,0 +1,111 @@
+var config_0000000092a0bc9c = {
+  "type": "serial",
+  "addClassNames": true,
+  "theme": "light",
+    "legend": {
+        "equalWidths": false,
+        "useGraphSettings": true,
+        "valueAlign": "left",
+        "valueWidth": 120
+    },
+  "balloon": {
+    "adjustBorderColor": false,
+    "horizontalPadding": 10,
+    "verticalPadding": 8,
+    "color": "#ffffff"
+  },
+  "balloonDateFormat" : "JJ:NN",
+
+  "dataProvider": data_0000000092a0bc9c,
+  /*
+  "dataLoader": {
+    "url": 'http://'+location.host+'/appeng/amchart/3/',
+  },*/
+  "valueAxes": [{
+	  "id": "energyAxis",           
+	  "axisAlpha": 0,
+	  "gridAlpha":0,
+	  "position": "left",
+	  "title": "Hourly Energy(Wh)",
+	  "stackType": "regular", 
+     },
+    /*{
+     "id": "uvAxis",           
+     "axisAlpha": 0,
+     "gridAlpha":0,
+     "position": "right",
+     "title": "UV",
+     
+    },*/
+    ],
+  "startDuration": 1,
+  "graphs": [{
+		"alphaField": "alpha",
+		"balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+		"fillAlphas": 1,
+		"type": "column",
+		"title": "PVI(1)",
+		"valueField": "1",
+		"valueAxis": "energyAxis",
+		"dashLengthField": "dashLengthColumn"
+  	},
+  	{
+	    "alphaField": "alpha",
+	    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+	    "fillAlphas": 1,
+	    "type": "column",
+	    "title": "PVI(2)",
+	    "valueField": "2",
+	    "valueAxis": "energyAxis",
+	    "dashLengthField": "dashLengthColumn"
+	},
+	/*{
+    "id": "graph3",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]](max)</span> [[additional]]</span>",
+    "bullet": "triangleUp",
+    "lineThickness": 2,
+    "bulletSize": 7,
+    "bulletBorderAlpha": 1,
+    "bulletColor": "#A9F5A9",
+                //"fillColors" : "#A9F5A9",
+    "useLineColorForBulletBorder": true,
+    "bulletBorderThickness": 3,
+    "fillAlphas": 0,
+    "lineAlpha": 1,
+    "title": "UV",
+    "valueField": "uv",
+    "valueAxis": "uvAxis",
+	},*/
+	],
+  "dataDateFormat": "YYYY-MM-DD JJ:NN:SS",
+  "categoryField": "date",
+  "categoryAxis": {
+    "dateFormats": [{
+      		"period":'hh', 
+      		"format":'JJ:NN'
+    	}, {
+          	"period": 'JJ', 
+          	"format": 'JJ:NN'
+    	}, {
+            "period": "DD",
+            "format": "MMM DD"
+        }, {
+            "period": "WW",
+            "format": "MMM DD"
+        }, {
+            "period": "MM",
+            "format": "MMM"
+        }, {
+            "period": "YYYY",
+            "format": "YYYY"
+        }],
+    "parseDates": true,
+    "minPeriod" : "hh",
+    "gridPosition": "start",
+    "axisAlpha": 0,
+    "tickLength": 0
+  },
+  "export": {
+    "enabled": true
+  }
+};
