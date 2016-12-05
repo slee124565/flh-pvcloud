27d5b4a068aaa26c1f8eed0d572951df38ff5a38
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-05 14:24:20 +0800

    text revised

--- static/scripts/services/pvserver.js
+++ static/scripts/services/pvserver.js
@@ -8,10 +8,12 @@
  * Service in the pvcApp.
  */
 angular.module('pvcApp')
-  .constant('apiBaseURL', 'http://localhost:8000/')
+  //.constant('apiBaseURL', 'http://localhost:8000/')
+  .constant('apiBaseURL', '/')
   .service('PVServer', [ '$http','apiBaseURL', function ($http, apiBaseURL) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 	  this.getPVSMeta = function(serial) {
+		  console.log('apiBaseURL: ' + apiBaseURL);
 		return $http.get( apiBaseURL + 'uapp/api/' + serial);
 	  };
   }]);
